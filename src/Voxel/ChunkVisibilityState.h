8eafb71d73db658ba3ef86ae9e87d18e1cc8f248
115denis511 <115denis511@gmail.com>
2025-01-03 05:18:29 +0200

    moved constexpr array init function definition from header file to lambda

--- src/Voxel/ChunkVisibilityState.h
+++ src/Voxel/ChunkVisibilityState.h
@@ -27,49 +27,6 @@ namespace engine {
         bool isVisible(uint32_t side) const { return (m_visibilityState & (0b1 << side)) != 0; }
         void findVisibleCornersAndEdges();
         uint32_t getSidesCount() const { return 26U; }
-        static constexpr std::array<uint32_t, 64> getCornersAndEdgesData() {
-            std::array<uint32_t, 64> table;
-
-            uint32_t leftBit = sideBit(Side::LEFT_FACE);
-            uint32_t rightBit = sideBit(Side::RIGHT_FACE);
-            uint32_t topBit = sideBit(Side::TOP_FACE);
-            uint32_t bottomBit = sideBit(Side::BOTTOM_FACE);
-            uint32_t backBit = sideBit(Side::BACK_FACE);
-            uint32_t frontBit = sideBit(Side::FRONT_FACE);
-
-            for (uint32_t i = 0; i < 64U; i++) {
-                uint32_t result = i;
-
-                if ((i & (leftBit | frontBit)) == (leftBit | frontBit)) result |= sideBit(Side::LEFT_FRONT_EDGE);
-                if ((i & (rightBit | frontBit)) == (rightBit | frontBit)) result |= sideBit(Side::RIGHT_FRONT_EDGE);
-                if ((i & (leftBit | backBit)) == (leftBit | backBit)) result |= sideBit(Side::LEFT_BACK_EDGE);
-                if ((i & (rightBit | backBit)) == (rightBit | backBit)) result |= sideBit(Side::RIGHT_BACK_EDGE);
-                
-                if ((i & (topBit | frontBit)) == (topBit | frontBit)) result |= sideBit(Side::TOP_FRONT_EDGE);
-                if ((i & (topBit | rightBit)) == (topBit | rightBit)) result |= sideBit(Side::TOP_RIGHT_EDGE);
-                if ((i & (topBit | backBit)) == (topBit | backBit)) result |= sideBit(Side::TOP_BACK_EDGE);
-                if ((i & (topBit | leftBit)) == (topBit | leftBit)) result |= sideBit(Side::TOP_LEFT_EDGE);
-                
-                if ((i & (bottomBit | frontBit)) == (bottomBit | frontBit)) result |= sideBit(Side::BOTTOM_FRONT_EDGE);
-                if ((i & (bottomBit | rightBit)) == (bottomBit | rightBit)) result |= sideBit(Side::BOTTOM_RIGHT_EDGE);
-                if ((i & (bottomBit | backBit)) == (bottomBit | backBit)) result |= sideBit(Side::BOTTOM_BACK_EDGE);
-                if ((i & (bottomBit | leftBit)) == (bottomBit | leftBit)) result |= sideBit(Side::BOTTOM_LEFT_EDGE);
-
-                if ((i & (leftBit | topBit | frontBit)) == (leftBit | topBit | frontBit)) result |= sideBit(Side::LEFT_TOP_FRONT_CORNER);
-                if ((i & (rightBit | topBit | frontBit)) == (rightBit | topBit | frontBit)) result |= sideBit(Side::RIGHT_TOP_FRONT_CORNER);
-                if ((i & (rightBit | topBit | backBit)) == (rightBit | topBit | backBit)) result |= sideBit(Side::RIGHT_TOP_BACK_CORNER);
-                if ((i & (leftBit | topBit | backBit)) == (leftBit | topBit | backBit)) result |= sideBit(Side::LEFT_TOP_BACK_CORNER);
-                
-                if ((i & (leftBit | bottomBit | frontBit)) == (leftBit | bottomBit | frontBit)) result |= sideBit(Side::LEFT_BOTTOM_FRONT_CORNER);
-                if ((i & (rightBit | bottomBit | frontBit)) == (rightBit | bottomBit | frontBit)) result |= sideBit(Side::RIGHT_BOTTOM_FRONT_CORNER);
-                if ((i & (rightBit | bottomBit | backBit)) == (rightBit | bottomBit | backBit)) result |= sideBit(Side::RIGHT_BOTTOM_BACK_CORNER);
-                if ((i & (leftBit | bottomBit | backBit)) == (leftBit | bottomBit | backBit)) result |= sideBit(Side::LEFT_BOTTOM_BACK_CORNER);
-
-                table[i] = result;
-            }
-
-            return table;
-        }
         static constexpr uint32_t sideBit(Side side) { return 0b1 << static_cast<uint32_t>(side); }
 
     private:  
